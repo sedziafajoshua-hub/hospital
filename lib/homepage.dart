147fd55c9741379c36f3e61c388cf95f54f84440
JOSHUA <sedziafajoshua@gmail.com>
2026-07-30 14:31:45 +0000

    FIRST UPDATE

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,7 +4,7 @@ import 'package:hospital/nav/nav%202.dart';
 import 'package:hospital/nav/nav%203.dart';
 import 'package:hospital/nav/nav%204.dart';
 import 'package:hospital/nav/nav%205.dart';
-import 'package:hospital/nav/nav%206.dart';
+
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -25,43 +25,47 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          onTap:
-              (int newnumber){
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+            onTap:
+                (int newnumber){
 
-            setState(() {
-              currentpage = newnumber;
-            });
+              setState(() {
+                currentpage = newnumber;
+              });
 
-          },
-          items: [
-        BottomNavigationBarItem(icon: Icon(
-          color: Colors.grey,
-            Icons.add_ic_call_rounded),
-          label: ''
-        ),
-        BottomNavigationBarItem(icon: Icon(
-          color: Colors.grey,
-            Icons.contact_phone_sharp),
+            },
+            backgroundColor: Colors.yellow,
+            items: [
+          BottomNavigationBarItem(icon: Icon(
+            color: Colors.grey,
+              Icons.add_ic_call_rounded),
             label: ''
-        ),
-        BottomNavigationBarItem(icon: Icon(
-          color: Colors.grey,
-            Icons.medical_services_rounded),
-            label: ''
-        ),
-        BottomNavigationBarItem(icon: Icon(
-          color: Colors.grey,
-            Icons.monitor_heart_outlined),
-            label: ''
-        ),
-        BottomNavigationBarItem(icon: Icon(
-          color: Colors.grey,
-            Icons.account_circle),
-            label: ''
-        ),
+          ),
+          BottomNavigationBarItem(icon: Icon(
+            color: Colors.grey,
+              Icons.library_books),
+              label: ''
+          ),
+          BottomNavigationBarItem(icon: Icon(
+            color: Colors.grey,
+              Icons.medical_services_rounded),
+              label: ''
+          ),
+          BottomNavigationBarItem(icon: Icon(
+            color: Colors.grey,
+              Icons.monitor_heart_outlined),
+              label: ''
+          ),
+          BottomNavigationBarItem(icon: Icon(
+            color: Colors.grey,
+              Icons.account_circle),
+              label: ''
+          ),
 
-      ]),
+        ]),
+      ),
       body:_page[
         currentpage
       ],
--- lib/nav/nav 5.dart
+++ lib/nav/nav 5.dart
@@ -10,6 +10,288 @@ class Nav5 extends StatefulWidget {
 class _Nav5State extends State<Nav5> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 180,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30)
+                )
+            ),
+            child: Column(
+              crossAxisAlignment:.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: .end,
+                    children: [
+                      Icon(
+                        size: 30,
+                          color: Colors.white,
+                          Icons.settings
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        foregroundImage: AssetImage('asset/image/girl.png'),
+                        radius: 35,
+                        backgroundColor: Colors.redAccent,
+                      ),
+                      Text('data'),
+                      Text(
+                        'Midwife'
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: Column(
+              children: [Container(
+                height: 400,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20)
+                  ),
+                  color: Colors.white
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue.shade50
+                            ),
+                            child: Icon(
+                              color: Colors.deepPurple,
+                                size: 30,
+                                Icons.person_outline),
+                          ),
+                          SizedBox(
+                            width:10,
+                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold
+                                ),
+                                'My Profile'
+                              ),
+                              Text(
+                                'View and edit your profile'
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 200,
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                      Divider(
+                        thickness: 1,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue.shade50
+                            ),
+                            child: Icon(
+                              color: Colors.deepPurple,
+                              size: 30,
+                                Icons.local_hospital),
+                          ),
+                          SizedBox(
+                            width:10,
+                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  'Clinic Information'
+                              ),
+                              Text(
+                                  'Manage Clinic details'
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 220,
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                      Divider(
+                        thickness: 1,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.orange.shade50
+                            ),
+                            child: Icon(
+                              color: Colors.orange,
+                                size: 26,
+                                Icons.notifications_none),
+                          ),
+                          SizedBox(
+                            width:10,
+                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  'Notifications'
+                              ),
+                              Text(
+                                  'Manage your notifications'
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 195,
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                      Divider(
+                        thickness: 1,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue.shade50
+                            ),
+                            child: Icon(
+                              color: Colors.green,
+                                size: 26,
+                                Icons.settings_outlined),
+                          ),
+                          SizedBox(
+                            width:10,
+                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  'Settings'
+                              ),
+                              Text(
+                                  'App preferences'
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 260,
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                      Divider(
+                        thickness: 1,
+                        height: 30,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue.shade50
+                            ),
+                            child: Icon(
+                              color: Colors.deepPurple,
+                                size: 26,
+                                Icons.help_outline_outlined),
+                          ),
+                          SizedBox(
+                            width:10,
+                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                  'Help & Support'
+                              ),
+                              Text(
+                                  'Get help and support'
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 230,
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                    ],
+                  )
+                ),
+              )],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
